060839083afb63f94274765b97dbdd24c04edeb3
Juno Kim <lippoint.surf0622@gmail.com>
2022-07-21 14:22:33 +0900

    코드수정

--- src/components/StoreTime.js
+++ src/components/StoreTime.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   View,
   Text,
@@ -6,45 +6,49 @@ import {
   ActivityIndicator,
   SafeAreaView,
   Image,
-  Alert,
-} from "react-native";
-import {useSelector, useDispatch} from "react-redux";
-import Api from "../Api";
-import BaseStyle, {Primary} from "../styles/Base";
-import moment from "moment";
-import "moment/locale/ko";
-import * as storeTimeAction from "../redux/actions/storeTimeAction";
-import AnimateLoading from "./AnimateLoading";
+  Alert
+} from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import Api from '../Api';
+import BaseStyle, { Primary } from '../styles/Base';
+import moment from 'moment';
+import 'moment/locale/ko';
+import * as storeTimeAction from '../redux/actions/storeTimeAction';
+import AnimateLoading from './AnimateLoading';
 
 const StoreTime = props => {
-  const navigation = props.navigation;
-  const {mt_id, mt_jumju_code} = useSelector(state => state.login);
-  const {storeTime} = useSelector(state => state.storeTime);
+  const navigation = props.navigation
+  const { mt_id, mt_jumju_code } = useSelector(state => state.login)
+  const { storeTime } = useSelector(state => state.storeTime)
 
-  const [isLoading, setLoading] = React.useState(false);
-  const [storeTimeList, setStoreTimeList] = React.useState(null); // 영업시간
+  const [isLoading, setLoading] = React.useState(false)
+  const [storeTimeList, setStoreTimeList] = React.useState(null) // 영업시간
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const getStoreTimeHandler = () => {
     const param = {
       encodeJson: true,
       jumju_id: mt_id,
       jumju_code: mt_jumju_code,
-      mode: "list",
-    };
-    Api.send("store_service_hour", param, args => {
-      const resultItem = args.resultItem;
-      let arrItems = args.arrItems;
+      mode: 'list'
+    }
+    Api.send('store_service_hour', param, args => {
+      const resultItem = args.resultItem
+      const arrItems = args.arrItems
 
-      if (resultItem.result === "Y") {
-        setStoreTimeList(arrItems);
-        dispatch(storeTimeAction.updateStoreTime(JSON.stringify(arrItems)));
+      console.log('resultItem', resultItem)
+
+      if (resultItem.result === 'Y') {
+        setStoreTimeList(arrItems)
+        dispatch(storeTimeAction.updateStoreTime(JSON.stringify(arrItems)))
       } else {
-        setStoreTimeList(null);
-        dispatch(storeTimeAction.updateStoreTime(JSON.stringify(arrItems)));
+        setStoreTimeList(null)
+        dispatch(storeTimeAction.updateStoreTime(JSON.stringify(arrItems)))
       }
-    });
+
+      setLoading(false)
+    })
   };
 
   const delStoreTimeHandler = st_idx => {
@@ -53,72 +57,72 @@ const StoreTime = props => {
       jumju_id: mt_id,
       jumju_code: mt_jumju_code,
       st_idx,
-      mode: "delete",
-    };
-    Api.send("store_service_hour", param, args => {
-      const resultItem = args.resultItem;
-      let arrItems = args.arrItems;
+      mode: 'delete'
+    }
+    Api.send('store_service_hour', param, args => {
+      const resultItem = args.resultItem
+      const arrItems = args.arrItems
 
-      if (resultItem.result === "Y") {
-        getStoreTimeHandler();
+      if (resultItem.result === 'Y') {
+        getStoreTimeHandler()
       } else {
-        Alert.alert("영업시간을 삭제할 수 없습니다.", "다시 한번 확인해주세요.", [
+        Alert.alert('영업시간을 삭제할 수 없습니다.', '다시 한번 확인해주세요.', [
           {
-            text: "확인",
-          },
-        ]);
+            text: '확인'
+          }
+        ])
       }
-    });
+    })
   };
 
   React.useEffect(() => {
-    const unsubscribe = navigation.addListener("focus", () => {
-      getStoreTimeHandler();
-    });
-    return unsubscribe;
-  }, [navigation]);
+    const unsubscribe = navigation.addListener('focus', () => {
+      getStoreTimeHandler()
+    })
+    return unsubscribe
+  }, [navigation])
 
   return (
     <SafeAreaView>
-      <View style={{...BaseStyle.pv15, backgroundColor: "#F8F8F8", ...BaseStyle.ph20}}>
-        <Text style={{...BaseStyle.ko15, ...BaseStyle.font_bold}}>영업시간</Text>
+      <View style={{ ...BaseStyle.pv15, backgroundColor: '#F8F8F8', ...BaseStyle.ph20 }}>
+        <Text style={{ ...BaseStyle.ko15, ...BaseStyle.font_bold }}>영업시간</Text>
       </View>
       {isLoading ? (
-        <AnimateLoading description="잠시만 기다려주세요." />
+        <AnimateLoading description='' />
       ) : (
-        <View style={{...BaseStyle.ph20, ...BaseStyle.mv15}}>
+        <View style={{ ...BaseStyle.ph20, ...BaseStyle.mv15 }}>
           {/* 영업시간 리스트 */}
           {storeTime &&
             storeTime.map((item, index) => (
-              <View key={item.st_idx} style={{...BaseStyle.container5, ...BaseStyle.mv10}}>
-                <View style={{...BaseStyle.container}}>
-                  <Text style={{...BaseStyle.ko14, ...BaseStyle.font_bold, ...BaseStyle.mr10}}>
+              <View key={item.st_idx} style={{ ...BaseStyle.container5, ...BaseStyle.mv10 }}>
+                <View style={{ ...BaseStyle.container }}>
+                  <Text style={{ ...BaseStyle.ko14, ...BaseStyle.font_bold, ...BaseStyle.mr10 }}>
                     {item.st_yoil_txt}
                   </Text>
-                  <Text style={{...BaseStyle.ko14}}>{`${item.st_stime} ~ ${item.st_etime}`}</Text>
+                  <Text style={{ ...BaseStyle.ko14 }}>{`${item.st_stime} ~ ${item.st_etime}`}</Text>
                 </View>
                 <TouchableOpacity
                   activeOpacity={1}
                   onPress={() =>
                     Alert.alert(
-                      "해당 영업시간을 삭제하시겠습니까?",
-                      "삭제하시면 복구하실 수 없습니다.",
+                      '해당 영업시간을 삭제하시겠습니까?',
+                      '삭제하시면 복구하실 수 없습니다.',
                       [
                         {
-                          text: "예",
-                          onPress: () => delStoreTimeHandler(item.st_idx),
+                          text: '예',
+                          onPress: () => delStoreTimeHandler(item.st_idx)
                         },
                         {
-                          text: "아니오",
-                        },
-                      ],
-                    )
-                  }
-                  hitSlop={{top: 10, right: 10, bottom: 10, left: 10}}>
+                          text: '아니오'
+                        }
+                      ]
+                    )}
+                  hitSlop={{ top: 10, right: 10, bottom: 10, left: 10 }}
+                >
                   <Image
-                    source={require("../images/popup_close.png")}
-                    style={{width: 18, height: 18, borderRadius: 18, opacity: 0.5}}
-                    resizeMode="cover"
+                    source={require('../images/popup_close.png')}
+                    style={{ width: 18, height: 18, borderRadius: 18, opacity: 0.5 }}
+                    resizeMode='cover'
                   />
                 </TouchableOpacity>
               </View>
@@ -127,22 +131,24 @@ const StoreTime = props => {
 
           <TouchableOpacity
             activeOpacity={1}
-            onPress={() => navigation.navigate("Home", {screen: "SetTime"})}
-            style={{...BaseStyle.mainBtn, ...BaseStyle.mv10}}>
+            onPress={() => navigation.navigate('Home', { screen: 'SetTime' })}
+            style={{ ...BaseStyle.mainBtn, ...BaseStyle.mv10 }}
+          >
             <Text
               style={{
                 ...BaseStyle.ko15,
                 ...BaseStyle.font_bold,
                 ...BaseStyle.font_222,
-                ...BaseStyle.textWhite,
-              }}>
+                ...BaseStyle.textWhite
+              }}
+            >
               영업시간 추가
             </Text>
           </TouchableOpacity>
         </View>
       )}
     </SafeAreaView>
-  );
+  )
 };
 
-export default StoreTime;
+export default StoreTime
