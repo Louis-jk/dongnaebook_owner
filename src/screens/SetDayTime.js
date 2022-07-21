1f35a68b5a98d881d80aed2bb2d149380e54472e
Juno Kim <lippoint.surf0622@gmail.com>
2022-07-21 14:22:41 +0900

    코드수정

--- src/screens/SetDayTime.js
+++ src/screens/SetDayTime.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import {
   View,
   Text,
@@ -9,69 +9,69 @@ import {
   ScrollView,
   FlatList,
   Dimensions,
-  BackHandler,
-} from "react-native";
-import DropDownPicker from "react-native-dropdown-picker";
-import Header from "../components/SubHeader";
-import BaseStyle, {Primary} from "../styles/Base";
-import {Calendar, CalendarList, Agenda, LocaleConfig} from "react-native-calendars";
-import {useSelector, useDispatch} from "react-redux";
-import Api from "../Api";
-import StoreTime from "../components/StoreTime";
-import StoreRegularHoliday from "../components/StoreRegularHoliday";
-import * as closedDayAction from "../redux/actions/closedDayAction";
+  BackHandler
+} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import Header from '../components/SubHeader';
+import BaseStyle, { Primary } from '../styles/Base';
+import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
+import { useSelector, useDispatch } from 'react-redux';
+import Api from '../Api';
+import StoreTime from '../components/StoreTime';
+import StoreRegularHoliday from '../components/StoreRegularHoliday';
+import * as closedDayAction from '../redux/actions/closedDayAction';
 
-export function ListCheckbox(props) {
-  return <View />;
+export function ListCheckbox (props) {
+  return <View />
 }
 
 const SetDayTime = props => {
-  const [listCheckbox, setListCheckbox] = React.useState();
+  const [listCheckbox, setListCheckbox] = React.useState()
 
-  const {navigation} = props;
-  const {mt_id, mt_jumju_code} = useSelector(state => state.login);
-  const {markedDay} = useSelector(state => state.closedDay);
+  const { navigation } = props
+  const { mt_id, mt_jumju_code } = useSelector(state => state.login)
+  const { markedDay } = useSelector(state => state.closedDay)
 
   // 안드로이드 뒤로가기 버튼 제어
   const backAction = () => {
-    navigation.goBack();
+    navigation.goBack()
 
-    return true;
+    return true
   };
 
   React.useEffect(() => {
-    BackHandler.addEventListener("hardwareBackPress", backAction);
-    return () => BackHandler.removeEventListener("hardwareBackPress", backAction);
-  }, []);
+    BackHandler.addEventListener('hardwareBackPress', backAction)
+    return () => BackHandler.removeEventListener('hardwareBackPress', backAction)
+  }, [])
 
   // 휴무일 마킹
-  const [marking, setMarking] = React.useState({});
+  const [marking, setMarking] = React.useState({})
 
   const markDate = day => {
-    const markDates = {};
-    markDates[day] = {selected: true, selectedColor: Primary.PointColor01, marked: true};
-    setMarking(markDates);
+    const markDates = {}
+    markDates[day] = { selected: true, selectedColor: Primary.PointColor01, marked: true }
+    setMarking(markDates)
   };
 
   LocaleConfig.locales.kr = {
     monthNames: [
-      "1월",
-      "2월",
-      "3월",
-      "4월",
-      "5월",
-      "6월",
-      "7월",
-      "8월",
-      "9월",
-      "10월",
-      "11월",
-      "12월",
+      '1월',
+      '2월',
+      '3월',
+      '4월',
+      '5월',
+      '6월',
+      '7월',
+      '8월',
+      '9월',
+      '10월',
+      '11월',
+      '12월'
     ],
-    dayNames: ["일", "월", "화", "수", "목", "금", "토"],
-    dayNamesShort: ["일", "월", "화", "수", "목", "금", "토"],
-  };
-  LocaleConfig.defaultLocale = "kr";
+    dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토']
+  }
+  LocaleConfig.defaultLocale = 'kr';
 
   const getHolidayAllListHandler = () => {
     // setCaIsLoading(false);
@@ -79,64 +79,64 @@ const SetDayTime = props => {
       encodeJson: true,
       jumju_id: mt_id,
       jumju_code: mt_jumju_code,
-      mode: "list",
-    };
+      mode: 'list'
+    }
 
-    Api.send("store_hoilday", param, args => {
-      const resultItem = args.resultItem;
-      let arrItems = args.arrItems;
+    Api.send('store_hoilday', param, args => {
+      const resultItem = args.resultItem
+      let arrItems = args.arrItems
 
-      if (resultItem.result === "Y") {
-        const markDates = {};
+      if (resultItem.result === 'Y') {
+        const markDates = {}
         arrItems.map(date => {
           markDates[date.sh_date] = {
             selected: true,
             selectedColor: Primary.PointColor01,
-            marked: true,
-          };
-          let newObj = Object.assign(marking, markDates);
-          setMarking(newObj);
+            marked: true
+          }
+          let newObj = Object.assign(marking, markDates)
+          setMarking(newObj)
 
-          const rs2 = <ListCheckbox checked items={1} />; // 리렌더링 (캘린더 멀티마킹 실시간 반영 안됨으로 강제 리렌더링)
-          setListCheckbox(rs2);
+          const rs2 = <ListCheckbox checked items={1} /> // 리렌더링 (캘린더 멀티마킹 실시간 반영 안됨으로 강제 리렌더링)
+          setListCheckbox(rs2)
           // dispatch(closedDayAction.updateClosedDay(JSON.stringify(newObj)));
-        });
+        })
       } else {
-        const markDates = {};
+        const markDates = {}
         arrItems.map(date => {
           markDates[date.sh_date] = {
             selected: true,
             selectedColor: Primary.PointColor01,
-            marked: true,
-          };
-          let newObj = Object.assign(marking, markDates);
-          setMarking(newObj);
+            marked: true
+          }
+          let newObj = Object.assign(marking, markDates)
+          setMarking(newObj)
 
-          const rs2 = <ListCheckbox checked items={1} />; // 리렌더링 (캘린더 멀티마킹 실시간 반영 안됨으로 강제 리렌더링)
-          setListCheckbox(rs2);
+          const rs2 = <ListCheckbox checked items={1} /> // 리렌더링 (캘린더 멀티마킹 실시간 반영 안됨으로 강제 리렌더링)
+          setListCheckbox(rs2)
           // dispatch(closedDayAction.updateClosedDay(JSON.stringify(newObj)));
-        });
+        })
       }
-    });
+    })
   };
 
   const setMarkedDays = payload => {
-    selectHolidayHandler(payload);
+    selectHolidayHandler(payload)
 
-    let markDates = {};
-    markDates[payload] = {selected: true, selectedColor: Primary.PointColor01, marked: true};
+    let markDates = {}
+    markDates[payload] = { selected: true, selectedColor: Primary.PointColor01, marked: true }
 
-    let check = marking.hasOwnProperty(payload); // 마킹 포함 여부(Object)
+    let check = marking.hasOwnProperty(payload) // 마킹 포함 여부(Object)
 
     if (check) {
       // 마킹 안 포함일 경우
-      delete marking[payload]; // 해당 마킹 Obj 삭제
+      delete marking[payload] // 해당 마킹 Obj 삭제
     } else {
-      const newObj = Object.assign(marking, markDates);
-      setMarking(newObj);
+      const newObj = Object.assign(marking, markDates)
+      setMarking(newObj)
     }
-    const rs2 = <ListCheckbox checked items={1} />; // 리렌더링 (캘린더 멀티마킹 실시간 반영 안됨으로 강제 리렌더링)
-    setListCheckbox(rs2);
+    const rs2 = <ListCheckbox checked items={1} /> // 리렌더링 (캘린더 멀티마킹 실시간 반영 안됨으로 강제 리렌더링)
+    setListCheckbox(rs2)
   };
 
   const selectHolidayHandler = payload => {
@@ -145,62 +145,62 @@ const SetDayTime = props => {
       jumju_id: mt_id,
       jumju_code: mt_jumju_code,
       sh_date: payload,
-      mode: "update",
-    };
-    Api.send("store_hoilday", param, args => {
-      const resultItem = args.resultItem;
-      let arrItems = args.arrItems;
+      mode: 'update'
+    }
+    Api.send('store_hoilday', param, args => {
+      const resultItem = args.resultItem
+      let arrItems = args.arrItems
 
-      if (resultItem.result === "Y") {
+      if (resultItem.result === 'Y') {
         // getHolidayAllListHandler();
-        console.log("====================================");
-        console.log("resultItem 캘린더 ::: ", resultItem);
-        console.log("arrItems 캘린더 ::: ", arrItems);
-        console.log("====================================");
+        console.log('====================================')
+        console.log('resultItem 캘린더 ::: ', resultItem)
+        console.log('arrItems 캘린더 ::: ', arrItems)
+        console.log('====================================')
       } else {
-        console.log("선택 날짜 ? ", arrItems);
+        console.log('선택 날짜 ? ', arrItems)
         // Alert.alert('데이터를 받아오는데 오류가 발생하였습니다.','관리자에게 문의해주세요.', [
         //   {
         //     text: '확인'
         //   }
         // ]);
       }
-    });
+    })
   };
 
   React.useEffect(() => {
-    getHolidayAllListHandler();
-  }, []);
+    getHolidayAllListHandler()
+  }, [])
 
   const _renderArrow = direction => {
     return (
       <Image
         source={
-          direction === "left"
-            ? require("../images/pg_prev02.png")
-            : require("../images/pg_next02.png")
+          direction === 'left'
+            ? require('../images/pg_prev02.png')
+            : require('../images/pg_next02.png')
         }
-        style={{width: 20, height: 20}}
-        resizeMode="contain"
+        style={{ width: 20, height: 20 }}
+        resizeMode='contain'
       />
-    );
+    )
   };
 
   return (
-    <View style={{flex: 1, backgroundColor: "#fff"}}>
-      <Header navigation={navigation} title="영업 운영 시간 설정" type="save" />
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <Header navigation={navigation} title='영업 운영 시간 설정' type='save' />
       {/* <StatusMenu navigation={navigation} /> */}
 
       <ScrollView>
         <StoreTime navigation={navigation} />
         <StoreRegularHoliday navigation={navigation} />
         {/* 휴무일 */}
-        <View style={{...BaseStyle.pv15, backgroundColor: "#F8F8F8", ...BaseStyle.ph20}}>
-          <Text style={{...BaseStyle.ko15, ...BaseStyle.font_bold}}>휴무일</Text>
+        <View style={{ ...BaseStyle.pv15, backgroundColor: '#F8F8F8', ...BaseStyle.ph20 }}>
+          <Text style={{ ...BaseStyle.ko15, ...BaseStyle.font_bold }}>휴무일</Text>
         </View>
-        <View style={{...BaseStyle.ph20, ...BaseStyle.mv15}}>
+        <View style={{ ...BaseStyle.ph20, ...BaseStyle.mv15 }}>
           {/* 캘린더 */}
-          <View style={{...BaseStyle.mv10}}>
+          <View style={{ ...BaseStyle.mv10 }}>
             <Calendar
               // markingType={'custom'}
               markedDates={marking}
@@ -209,9 +209,9 @@ const SetDayTime = props => {
               current={new Date()}
               onDayPress={day => setMarkedDays(day.dateString)}
               onDayLongPress={day => setMarkedDays(day.dateString)}
-              monthFormat="yyyy년 M월"
+              monthFormat='yyyy년 M월'
               onMonthChange={month => {
-                console.log("month changed", month);
+                console.log('month changed', month)
               }}
               hideArrows={false}
               renderArrow={_renderArrow}
@@ -228,41 +228,41 @@ const SetDayTime = props => {
               disableAllTouchEventsForDisabledDays
               enableSwipeMonths
               style={{
-                height: 350,
+                height: 350
               }}
               theme={{
-                "stylesheet.calendar.header": {
+                'stylesheet.calendar.header': {
                   dayTextAtIndex0: {
-                    color: "#d62828",
+                    color: '#d62828'
                   },
                   dayTextAtIndex6: {
-                    color: "#00509d",
-                  },
+                    color: '#00509d'
+                  }
                 },
-                backgroundColor: "#ffffff",
-                calendarBackground: "#ffffff",
+                backgroundColor: '#ffffff',
+                calendarBackground: '#ffffff',
                 textSectionTitleColor: Primary.PointColor02,
-                textSectionTitleDisabledColor: "#d9e1e8",
+                textSectionTitleDisabledColor: '#d9e1e8',
                 selectedDayBackgroundColor: Primary.PointColor01,
-                selectedDayTextColor: "#fff",
-                todayTextColor: "#20ABC8",
-                dayTextColor: "#222",
-                textDisabledColor: "#d9e1e8",
+                selectedDayTextColor: '#fff',
+                todayTextColor: '#20ABC8',
+                dayTextColor: '#222',
+                textDisabledColor: '#d9e1e8',
                 dotColor: Primary.PointColor01,
                 selectedDotColor: Primary.PointColor01,
-                arrowColor: "orange",
-                disabledArrowColor: "#d9e1e8",
-                monthTextColor: "#333",
-                indicatorColor: "blue",
-                textDayFontFamily: "monospace",
-                textMonthFontFamily: "monospace",
-                textDayHeaderFontFamily: "monospace",
-                textDayFontWeight: "300",
-                textMonthFontWeight: "bold",
-                textDayHeaderFontWeight: "300",
+                arrowColor: 'orange',
+                disabledArrowColor: '#d9e1e8',
+                monthTextColor: '#333',
+                indicatorColor: 'blue',
+                textDayFontFamily: 'monospace',
+                textMonthFontFamily: 'monospace',
+                textDayHeaderFontFamily: 'monospace',
+                textDayFontWeight: '300',
+                textMonthFontWeight: 'bold',
+                textDayHeaderFontWeight: '300',
                 textDayFontSize: 16,
                 textMonthFontSize: 20,
-                textDayHeaderFontSize: 16,
+                textDayHeaderFontSize: 16
               }}
             />
           </View>
@@ -275,7 +275,7 @@ const SetDayTime = props => {
         {/* //휴무일 */}
       </ScrollView>
     </View>
-  );
+  )
 };
 
-export default SetDayTime;
+export default SetDayTime
