d6f3572c259a3e228609ca0ab0aea154f85ad083
Juno Kim <lippoint.surf0622@gmail.com>
2022-07-22 16:25:35 +0900

    api 연동 부분 확인 및 주문 취소 모달 스타일 수정

--- src/components/OrderCheckModal.js
+++ src/components/OrderCheckModal.js
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import { View, Text, TouchableOpacity, TextInput, Image, Alert } from 'react-native';
-import Modal from 'react-native-modal';
-import { useSelector, useDispatch } from 'react-redux';
-import BaseStyle, { Primary } from '../styles/Base';
-import Api from '../Api';
-import * as orderAction from '../redux/actions/orderAction';
+import * as React from "react";
+import {View, Text, TouchableOpacity, TextInput, Image, Alert} from "react-native";
+import Modal from "react-native-modal";
+import {useSelector, useDispatch} from "react-redux";
+import BaseStyle, {Primary} from "../styles/Base";
+import Api from "../Api";
+import * as orderAction from "../redux/actions/orderAction";
 
-const OrderCheckModal = ({ isModalVisible, toggleModal, od_id, od_type, navigation }) => {
-  const [time01, setTime01] = React.useState('')
-  const [time02, setTime02] = React.useState('')
-  const deliveryTimeRef = React.useRef(null)
-  const { mt_id, mt_jumju_code } = useSelector(state => state.login)
-  const dispatch = useDispatch()
+const OrderCheckModal = ({isModalVisible, toggleModal, od_id, od_type, navigation}) => {
+  const [time01, setTime01] = React.useState("");
+  const [time02, setTime02] = React.useState("");
+  const deliveryTimeRef = React.useRef(null);
+  const {mt_id, mt_jumju_code} = useSelector(state => state.login);
+  const dispatch = useDispatch();
 
   const getOrderListHandler = () => {
     const param = {
@@ -20,19 +20,19 @@ const OrderCheckModal = ({ isModalVisible, toggleModal, od_id, od_type, navigati
       limit_count: 10,
       jumju_id: mt_id,
       jumju_code: mt_jumju_code,
-      od_process_status: '신규주문'
-    }
+      od_process_status: "신규주문",
+    };
 
-    Api.send('store_order_list', param, args => {
-      const resultItem = args.resultItem
-      let arrItems = args.arrItems
+    Api.send("store_order_list", param, args => {
+      const resultItem = args.resultItem;
+      let arrItems = args.arrItems;
 
-      if (resultItem.result === 'Y') {
-        dispatch(orderAction.updateNewOrder(JSON.stringify(arrItems)))
+      if (resultItem.result === "Y") {
+        dispatch(orderAction.updateNewOrder(JSON.stringify(arrItems)));
       } else {
-        dispatch(orderAction.updateNewOrder(null))
+        dispatch(orderAction.updateNewOrder(null));
       }
-    })
+    });
   };
 
   const checkOrderHandler = () => {
@@ -41,34 +41,36 @@ const OrderCheckModal = ({ isModalVisible, toggleModal, od_id, od_type, navigati
       od_id,
       jumju_id: mt_id,
       jumju_code: mt_jumju_code,
-      od_process_status: '접수완료',
+      od_process_status: "접수완료",
       delivery_time: time01,
-      visit_time: time02
-    }
+      visit_time: time02,
+    };
 
-    Api.send('store_order_status_update', param, args => {
-      const resultItem = args.resultItem
-      let arrItems = args.arrItems
-      if (resultItem.result === 'Y') {
-        getOrderListHandler()
-        toggleModal()
-        Alert.alert('주문을 접수하였습니다.', '', [
+    // proc_store_order_status_update
+    Api.send("store_order_status_update", param, args => {
+      const resultItem = args.resultItem;
+      let arrItems = args.arrItems;
+
+      if (resultItem.result === "Y") {
+        getOrderListHandler();
+        toggleModal();
+        Alert.alert("주문을 접수하였습니다.", "", [
           {
-            text: '확인',
-            onPress: () => navigation.navigate('Home', { screen: 'Main' })
-          }
-        ])
+            text: "확인",
+            onPress: () => navigation.navigate("Home", {screen: "Main"}),
+          },
+        ]);
       } else {
-        getOrderListHandler()
-        toggleModal()
-        Alert.alert('주문 접수중 오류가 발생하였습니다.', '다시 한번 시도해주세요.', [
+        getOrderListHandler();
+        toggleModal();
+        Alert.alert("주문 접수중 오류가 발생하였습니다.", "다시 한번 시도해주세요.", [
           {
-            text: '확인',
-            onPress: () => navigation.navigate('Home', { screen: 'Main' })
-          }
-        ])
+            text: "확인",
+            onPress: () => navigation.navigate("Home", {screen: "Main"}),
+          },
+        ]);
       }
-    })
+    });
   };
 
   return (
@@ -79,79 +81,75 @@ const OrderCheckModal = ({ isModalVisible, toggleModal, od_id, od_type, navigati
         // onBackdropPress={toggleModal}
         transparent
         statusBarTranslucent={false}
-        style={{ ...BaseStyle.ph10, ...BaseStyle.pv20 }}
-        animationIn='slideInUp'
-        animationInTiming={100}
-      >
+        style={{...BaseStyle.ph10, ...BaseStyle.pv20}}
+        animationIn="slideInUp"
+        animationInTiming={100}>
         <View
           style={{
-            position: 'relative',
-            backgroundColor: '#fff',
+            position: "relative",
+            backgroundColor: "#fff",
             ...BaseStyle.pv30,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 15
-          }}
-        >
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 15,
+          }}>
           <TouchableOpacity
             activeOpacity={1}
             onPress={toggleModal}
-            hitSlop={{ top: 10, right: 10, bottom: 10, left: 10 }}
+            hitSlop={{top: 10, right: 10, bottom: 10, left: 10}}
             style={{
-              position: 'absolute',
+              position: "absolute",
               top: -10,
               right: -10,
               backgroundColor: Primary.PointColor02,
               borderRadius: 50,
-              padding: 10
-            }}
-          >
+              padding: 10,
+            }}>
             <Image
-              source={require('../images/close_wh.png')}
-              style={{ width: 10, height: 10 }}
-              resizeMode='center'
+              source={require("../images/close_wh.png")}
+              style={{width: 10, height: 10}}
+              resizeMode="center"
             />
           </TouchableOpacity>
-          <Text style={{ ...BaseStyle.ko15, ...BaseStyle.mb15 }}>
-            {od_type === '배달' ? '배달 예상시간을 입력해주세요.' : '포장 예상시간을 입력해주세요.'}
+          <Text style={{...BaseStyle.ko15, ...BaseStyle.mb15}}>
+            {od_type === "배달" ? "배달 예상시간을 입력해주세요." : "포장 예상시간을 입력해주세요."}
           </Text>
-          <View style={{ width: '100%', ...BaseStyle.ph30 }}>
+          <View style={{width: "100%", ...BaseStyle.ph30}}>
             <View
               style={{
                 ...BaseStyle.container5,
                 ...BaseStyle.ph10,
                 ...BaseStyle.inputH,
-                ...BaseStyle.border
-              }}
-            >
+                ...BaseStyle.border,
+              }}>
               <TextInput
                 ref={deliveryTimeRef}
-                value={od_type === '배달' ? time01 : time02}
-                style={{ width: '83%', textAlign: 'right' }}
-                placeholder='예: 30'
+                value={od_type === "배달" ? time01 : time02}
+                style={{width: "83%", textAlign: "right"}}
+                placeholder="예: 30"
                 onChangeText={text => {
-                  const filteredText = text.replace(/(-)|(\.)/gi, '')
-                  if (filteredText !== null || filteredText !== '') {
-                    if (od_type === '배달') {
-                      setTime01(filteredText)
+                  const filteredText = text.replace(/(-)|(\.)/gi, "");
+                  if (filteredText !== null || filteredText !== "") {
+                    if (od_type === "배달") {
+                      setTime01(filteredText);
                     } else {
-                      setTime02(filteredText)
+                      setTime02(filteredText);
                     }
                   } else {
-                    if (od_type === '배달') {
-                      setTime01('0')
+                    if (od_type === "배달") {
+                      setTime01("0");
                     } else {
-                      setTime02('0')
+                      setTime02("0");
                     }
                   }
                 }}
-                autoCapitalize='none'
-                keyboardType='number-pad'
+                autoCapitalize="none"
+                keyboardType="number-pad"
               />
-              <Text>분 {od_type === '배달' ? '예상' : '후'}</Text>
+              <Text>분 {od_type === "배달" ? "예상" : "후"}</Text>
             </View>
           </View>
-          <View style={{ ...BaseStyle.container, ...BaseStyle.mt20, ...BaseStyle.ph30 }}>
+          <View style={{...BaseStyle.container, ...BaseStyle.mt20, ...BaseStyle.ph30}}>
             <TouchableOpacity
               activeOpacity={1}
               onPress={checkOrderHandler}
@@ -159,10 +157,9 @@ const OrderCheckModal = ({ isModalVisible, toggleModal, od_id, od_type, navigati
                 ...BaseStyle.mainBtn,
                 flex: 1,
                 ...BaseStyle.pv15,
-                borderRadius: 5
-              }}
-            >
-              <Text style={{ ...BaseStyle.ko15, ...BaseStyle.font_bold, ...BaseStyle.font_white }}>
+                borderRadius: 5,
+              }}>
+              <Text style={{...BaseStyle.ko15, ...BaseStyle.font_bold, ...BaseStyle.font_white}}>
                 전송하기
               </Text>
             </TouchableOpacity>
@@ -172,7 +169,7 @@ const OrderCheckModal = ({ isModalVisible, toggleModal, od_id, od_type, navigati
 
       {/* // 주문 접수 모달 */}
     </View>
-  )
+  );
 };
 
-export default OrderCheckModal
+export default OrderCheckModal;
