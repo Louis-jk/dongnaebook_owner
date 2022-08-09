b4198ce20cfe1e6ee17bdf576f50bd5299d1f50a
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-09 16:32:08 +0900

    test library 설치 및 설정

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  preset: "react-native",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|react-(native|universal|navigation)-(.*)|@jest|@react-native|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|(@[a-zA-Z]+/)?(bs|reason|rescript)-(.*)+)",
-  ],
-};
+  preset: '@testing-library/react-native',
+  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // transformIgnorePatterns: [
+  //   'node_modules/(?!(jest-)?react-native|react-(native|universal|navigation)-(.*)|@jest|@react-native|@react-native-community/(.*)|@react-navigation/(.*)|bs-platform|(@[a-zA-Z]+/)?(bs|reason|rescript)-(.*)+)'
+  // ],
+  transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\\/]+$'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect']
+  // setupFiles: ['node_modules/react-native-gesture-handler/jestSetup.js']
+}
