36690c31363ddc3c00b6e8b32b62cd65014a00ac
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-10 18:16:01 +0900

    신규 주문 saga Redux Store에 추가

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,13 +1,15 @@
-import reducers from "../reducers"
-import { createStore } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension"
-import createSagaMiddleware from "redux-saga"
+import reducers from '../reducers'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
+import mySaga from '../../sagas'
 // import logger from 'redux-logger';
 // import thunk from 'redux-thunk';
 
 const sagaMiddleware = createSagaMiddleware()
 
-export default function initStore() {
-  const store = createStore(reducers, composeWithDevTools())
+export default function initStore () {
+  const store = createStore(reducers, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+  sagaMiddleware.run(mySaga)
   return store
 }
