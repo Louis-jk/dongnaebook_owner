884e46850cb8056816d4deb0cdddba5c049c2b33
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-10 14:20:12 +0900

    레이아웃 처리 및 로딩 처리

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,17 +1,30 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-import AnimateLoading from '../components/AnimateLoading'
 
-const Layout = ({ isLoading, child, props }) => {
-  console.log('Layout isLoading', isLoading)
+const Layout = ({ child, props }) => {
+  // console.log('Layout isLoading', isLoading)
   console.log('Layout child', child)
   console.log('Layout props', props)
 
+  const [content, setContent] = React.useState(null)
+
+  const checkingCompoennt = React.useCallback((child) => {
+    setContent(child)
+  }, [child])
+
+  React.useEffect(() => {
+    let isSubscribed = true
+
+    if (isSubscribed) {
+      checkingCompoennt(child)
+    }
+
+    return () => {
+      isSubscribed = false
+    }
+  }, [])
+
   return (
-    <>
-      {isLoading && <AnimateLoading description='데이터를 불러오는 중입니다.' />}
-      {!isLoading && <>{child}</>}
-    </>
+    content
   )
 }
 
