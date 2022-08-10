9c1a7db0f9696a7a26f9f0830d17b2c252f2153f
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-10 10:39:54 +0900

    code convention

--- src/components/AnimateLoading.js
+++ src/components/AnimateLoading.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react"
-import { View, Text, Animated, Easing } from "react-native"
-import BaseStyle from "../styles/Base"
+import React, { useState } from 'react'
+import { View, Text, Animated, Easing } from 'react-native'
+import BaseStyle from '../styles/Base'
 
 const AnimateLoading = ({ description }) => {
   const Animation = useState(new Animated.Value(0))[0]
@@ -11,58 +11,58 @@ const AnimateLoading = ({ description }) => {
         toValue: 100,
         duration: 350,
         easing: Easing.ease,
-        useNativeDriver: true,
+        useNativeDriver: true
       }),
       Animated.timing(Animation, {
         toValue: 0,
         duration: 400,
         // delay: 800,
         // easing: Easing.cubic,
-        useNativeDriver: true,
-      }),
+        useNativeDriver: true
+      })
     ]),
     {
-      iterations: -1,
+      iterations: -1
     }
   ).start()
 
   const interpolated = Animation.interpolate({
     inputRange: [0, 20, 40, 60, 80, 100],
-    outputRange: ["0deg", "180deg", "270deg", "0deg", "-270deg", "-180deg"],
-    extrapolateLeft: "clamp",
+    outputRange: ['0deg', '180deg', '270deg', '0deg', '-270deg', '-180deg'],
+    extrapolateLeft: 'clamp'
   })
 
   const interpolatedY = Animation.interpolate({
     inputRange: [0, 100],
     outputRange: [-3, 3],
-    extrapolateLeft: "clamp",
+    extrapolateLeft: 'clamp'
   })
 
   const translate = Animation.interpolate({
     inputRange: [0, 20, 50],
     outputRange: [0, -10, -35],
-    extrapolateLeft: "clamp",
+    extrapolateLeft: 'clamp'
   })
 
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#fff",
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff'
       }}
     >
       <Animated.Image
-        source={require("../images/logo_mark.png")}
+        source={require('../images/logo_mark.png')}
         style={{
           width: 80,
           height: 80,
           marginBottom: 20,
           // transform: [{rotate: interpolated} /*  {translateY: translate} */],
-          transform: [{ translateY: interpolatedY }],
+          transform: [{ translateY: interpolatedY }]
         }}
-        resizeMode="cover"
+        resizeMode='cover'
       />
 
       {/* <ActivityIndicator size="large" color="#FCDC00" /> */}
