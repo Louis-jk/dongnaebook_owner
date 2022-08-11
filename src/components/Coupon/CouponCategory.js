16ac391dc9b1a7c00a04e6dbfc2f4bfb38572866
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-11 14:18:51 +0900

    쿠폰 카테고리 셀렉터 컴포넌트 분리

---
+++ src/components/Coupon/CouponCategory.js
@@ -0,0 +1,67 @@
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React from 'react'
+import RNPickerSelect from 'react-native-picker-select'
+import BaseStyle from '../../styles/Base'
+
+const CouponCategory = ({ type, setType }) => {
+  const customPickerStyles = StyleSheet.create({
+    inputIOS: {
+      fontSize: 14,
+      paddingVertical: 10,
+      paddingHorizontal: 12,
+      borderWidth: 1,
+      borderColor: '#E3E3E3',
+      borderRadius: 5,
+      height: 50,
+      color: 'black',
+      paddingRight: 30 // to ensure the text is never behind the icon
+    },
+    inputAndroid: {
+      fontSize: 14,
+      paddingHorizontal: 10,
+      paddingVertical: 8,
+      borderWidth: 1,
+      borderColor: '#E3E3E3',
+      borderRadius: 5,
+      height: 50,
+      color: 'black',
+      paddingRight: 30 // to ensure the text is never behind the icon
+    }
+  })
+
+  return (
+    <RNPickerSelect
+      fixAndroidTouchableBug
+      value={type}
+      useNativeAndroidPickerStyle={false}
+      placeholder={{ label: '선택해주세요.', value: null }}
+      onValueChange={value => setType(value)}
+      items={[
+        { label: '모두 사용가능', value: '0' },
+        { label: '포장용 쿠폰', value: '1' },
+        { label: '배달용 쿠폰', value: '2' }
+      ]}
+      style={{
+        ...customPickerStyles,
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        ...BaseStyle.round05,
+        ...BaseStyle.inputH,
+        placeholder: {
+          color: '#888'
+        }
+      }}
+      Icon={() => {
+        return (
+          <Image
+            source={require('../../images/ic_select.png')}
+            style={{ width: 45, height: 45 }}
+            resizeMode='center'
+          />
+        )
+      }}
+    />
+  )
+}
+
+export default CouponCategory
