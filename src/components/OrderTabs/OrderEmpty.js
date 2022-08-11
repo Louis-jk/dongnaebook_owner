0bb44febe3912ce612713f58dae7da05d9987195
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-11 17:31:20 +0900

    주문 빈 내역 로고 이미지 삽입 및 스타일 설정

--- src/components/OrderTabs/OrderEmpty.js
+++ src/components/OrderTabs/OrderEmpty.js
@@ -1,6 +1,6 @@
-import { View, Text, Dimensions } from 'react-native'
+import { View, Text, Dimensions, Image } from 'react-native'
 import * as React from 'react'
-import BaseStyle from '../../styles/Base'
+import BaseStyle, { Primary } from '../../styles/Base'
 
 export default function OrderEmpty ({ text }) {
   return (
@@ -9,10 +9,11 @@ export default function OrderEmpty ({ text }) {
         justifyContent: 'center',
         alignItems: 'center',
         flex: 1,
-        height: Dimensions.get('window').height - 300
+        height: Dimensions.get('window').height - 150
       }}
     >
-      <Text style={{ ...BaseStyle.ko15, textAlign: 'center' }}>
+      <Image source={require('../../images/logo_s.png')} style={{ width: 50, height: 50, opacity: 0.8, marginBottom: 10 }} resizeMode='cover' />
+      <Text style={{ ...BaseStyle.ko15, textAlign: 'center', color: Primary.PointColor01 }}>
         {`아직 ${text} 주문이 없습니다.`}
       </Text>
     </View>
