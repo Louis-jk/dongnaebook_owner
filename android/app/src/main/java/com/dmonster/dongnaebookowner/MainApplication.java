d551f3b59cafac62d31a7438c637e77afb3d5c59
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-17 11:52:17 +0900

    React Query 설치 및 link

--- android/app/src/main/java/com/dmonster/dongnaebookowner/MainApplication.java
+++ android/app/src/main/java/com/dmonster/dongnaebookowner/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.rnfs.RNFSPackage; // react-native-fs
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactInstanceManager;
