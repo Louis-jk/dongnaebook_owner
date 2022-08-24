aa684c27c0d57afbf9195d65507e3ff9e74bc556
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-24 15:27:34 +0900

    헤더 세로폭 스타일 수정

--- src/components/SetHeader.js
+++ src/components/SetHeader.js
@@ -1,58 +1,56 @@
-import React from "react"
-import { View, Text, Image, SafeAreaView, TouchableOpacity, Platform } from "react-native"
-import { DrawerActions } from "@react-navigation/native"
-import DropDownPicker from "react-native-dropdown-picker"
-import BaseStyle, { Primary } from "../styles/Base"
+import React from 'react'
+import { View, Text, Image, SafeAreaView, TouchableOpacity, Platform, Alert } from 'react-native'
+import { DrawerActions } from '@react-navigation/native'
+import BaseStyle from '../styles/Base'
 
 const SetHeader = props => {
   const { navigation, title, type, toggleModal } = props
 
   return (
     <SafeAreaView>
-      <View style={{ ...BaseStyle.container5, ...BaseStyle.ph20, paddingVertical: Platform.OS === 'ios' ? 10 : 20 }}>
+      <View style={{ ...BaseStyle.container5, ...BaseStyle.ph20, paddingVertical: Platform.OS === 'ios' ? 10 : 15 }}>
         <TouchableOpacity
           activeOpacity={1}
           onPress={() => navigation.goBack()}
           hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
         >
           <Image
-            source={require("../images/top_ic_history.png")}
+            source={require('../images/top_ic_history.png')}
             style={{ width: 30, height: 20 }}
-            resizeMode="contain"
+            resizeMode='contain'
           />
         </TouchableOpacity>
         <Text style={{ ...BaseStyle.ko18, ...BaseStyle.font_bold }}>{title}</Text>
-        {type === "add" || type === "save" ? (
+        {(type === 'add' || type === 'save') &&
           <TouchableOpacity
             activeOpacity={1}
             onPress={() => {
-              if (type === "add") {
+              if (type === 'add') {
                 toggleModal()
               } else {
-                alert("저장")
+                Alert.alert('저장')
               }
             }}
             hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
           >
             <Text style={{ ...BaseStyle.ko14, ...BaseStyle.font_bold }}>
-              {type === "add" ? "추가" : "저장"}
+              {type === 'add' ? '추가' : '저장'}
             </Text>
-          </TouchableOpacity>
-        ) : (
+          </TouchableOpacity>}
+        {(type !== 'add' && type !== 'save') &&
           <TouchableOpacity
             activeOpacity={1}
             onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
             hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
           >
             <Image
-              source={require("../images/ic_menu.png")}
+              source={require('../images/ic_menu.png')}
               style={{ width: 30, height: 30 }}
-              resizeMode="contain"
+              resizeMode='contain'
             />
-          </TouchableOpacity>
-        )}
+          </TouchableOpacity>}
       </View>
-      <View style={{ height: 1, width: "100%", backgroundColor: "#E3E3E3", ...BaseStyle.mb20 }} />
+      <View style={{ height: 1, width: '100%', backgroundColor: '#E3E3E3', marginBottom: type === 'add' || type === 'save' ? 20 : 0 }} />
     </SafeAreaView>
   )
 }
