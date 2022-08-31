084a2c657437c8a4b0db7a635d9b0b0a2b232bcc
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-31 16:30:55 +0900

    코드 수정

--- src/components/BusinessHoursAndHolidays/StoreTime.js
+++ src/components/BusinessHoursAndHolidays/StoreTime.js
@@ -45,8 +45,8 @@ const StoreTime = props => {
   }
 
   // st_yoil(요일 숫자(str)만 배열로 추출) 핸들러
-  const daysToArrayHandler = (arr) => {
-    const result = arr.reduce((acc, curr, _) => {
+  const daysToArrayHandler = (arrItems) => {
+    const result = arrItems.reduce((acc, curr, _) => {
       const toArr = curr.st_yoil.split(',')
       acc.push(toArr)
       return acc
