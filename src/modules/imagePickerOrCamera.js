4b34cb76a0bd67d5669f5fcfaabfc21b2c25e794
Juno Kim <lippoint.surf0622@gmail.com>
2022-09-27 10:06:37 +0900

    iOS 카메라, 갤러리 모달 닫힘 시간 이슈건 처리(첫번째 선택 후 부터 갤러리 | 카메라 모듈 바로 팅김현상)

--- src/modules/imagePickerOrCamera.js
+++ src/modules/imagePickerOrCamera.js
@@ -2,23 +2,50 @@ import ImagePicker from 'react-native-image-crop-picker' // 이미지 업로드 
 
 // 이미지 업로드
 export const pickGalleryImage = (setSource, setMenuImage, width = 400) => {
-  ImagePicker.openPicker({
-    mediaType: 'photo',
-    sortOrder: 'none',
-    width,
-    height: 300,
-    compressImageMaxWidth: 10000,
-    compressImageMaxHeight: 10000,
-    compressImageQuality: 1,
-    compressVideoPreset: 'MediumQuality',
-    includeExif: true,
-    cropperCircleOverlay: false,
-    useFrontCamera: false,
-    // includeBase64: true,
-    cropping: true
-  })
-    .then(img => {
-      // dispatch(UserProfileImg(img.path));
+  
+  setTimeout(() => {
+  
+    ImagePicker.openPicker({
+      mediaType: 'photo',
+      sortOrder: 'none',
+      width,
+      height: 300,
+      compressImageMaxWidth: 10000,
+      compressImageMaxHeight: 10000,
+      compressImageQuality: 1,
+      compressVideoPreset: 'MediumQuality',
+      includeExif: true,
+      cropperCircleOverlay: false,
+      useFrontCamera: false,
+      // includeBase64: true,
+      cropping: true
+    })
+      .then(img => {
+        // dispatch(UserProfileImg(img.path));
+        
+        setSource({
+          uri: img.path,
+          type: img.mime,
+          name: img.path.slice(img.path.lastIndexOf('/'))
+        })
+        setMenuImage(img.path)
+      })
+      .catch(e => console.log(e))
+
+  }, 1000)
+}
+
+
+// 카메라 촬영
+export const takeCamera = (setSource, setMenuImage) => {
+
+  setTimeout(() => {
+    
+    ImagePicker.openCamera({
+      width: 2000,
+      height: 1500,
+      cropping: true
+    }).then(img => {
       setSource({
         uri: img.path,
         type: img.mime,
@@ -26,22 +53,6 @@ export const pickGalleryImage = (setSource, setMenuImage, width = 400) => {
       })
       setMenuImage(img.path)
     })
-    .catch(e => console.log(e))
-}
 
-
-// 카메라 촬영
-export const takeCamera = (setSource, setMenuImage) => {
-  ImagePicker.openCamera({
-    width: 2000,
-    height: 1500,
-    cropping: true
-  }).then(img => {
-    setSource({
-      uri: img.path,
-      type: img.mime,
-      name: img.path.slice(img.path.lastIndexOf('/'))
-    })
-    setMenuImage(img.path)
-  })
+  }, 1000)
 }
