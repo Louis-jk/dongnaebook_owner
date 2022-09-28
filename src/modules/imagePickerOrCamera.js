46834b0625eebd502425c510df57428d4c683c69
Juno Kim <lippoint.surf0622@gmail.com>
2022-09-28 10:44:28 +0900

    optional height 추가 및 javascript VSCode 주석 추가

--- src/modules/imagePickerOrCamera.js
+++ src/modules/imagePickerOrCamera.js
@@ -1,7 +1,14 @@
 import ImagePicker from 'react-native-image-crop-picker' // 이미지 업로드 패키지
 
+/**
+ * Props 항목
+ * @param {React.Dispatch<React.SetStateAction<null>>} setSource 이미지 File객체를 담는 stateAction
+ * @param {React.Dispatch<React.SetStateAction<string>>} setMenuImage 이미지 패스만 담는 stateAction
+ * @param {number?} width optional 넓이 지정 (Default : 400)
+ * @param {number?} height optional 높이 지정 (Default : 300)
+ */
 // 이미지 업로드
-export const pickGalleryImage = (setSource, setMenuImage, width = 400) => {
+export const pickGalleryImage = (setSource, setMenuImage, width = 400, height = 300) => {
   
   setTimeout(() => {
   
@@ -9,7 +16,7 @@ export const pickGalleryImage = (setSource, setMenuImage, width = 400) => {
       mediaType: 'photo',
       sortOrder: 'none',
       width,
-      height: 300,
+      height,
       compressImageMaxWidth: 10000,
       compressImageMaxHeight: 10000,
       compressImageQuality: 1,
@@ -34,15 +41,21 @@ export const pickGalleryImage = (setSource, setMenuImage, width = 400) => {
   }, 1000)
 }
 
-
+/**
+ * Props 항목
+ * @param {React.Dispatch<React.SetStateAction<null>>} setSource 이미지 File객체를 담는 stateAction
+ * @param {React.Dispatch<React.SetStateAction<string>>} setMenuImage 이미지 패스만 담는 stateAction
+ * @param {number?} width optional 넓이 지정 (Default : 400)
+ * @param {number?} height optional 높이 지정 (Default : 300)
+ */
 // 카메라 촬영
-export const takeCamera = (setSource, setMenuImage) => {
+export const takeCamera = (setSource, setMenuImage, width = 400, height = 300) => {
 
   setTimeout(() => {
     
     ImagePicker.openCamera({
-      width: 2000,
-      height: 1500,
+      width, // 2000
+      height, // 1500
       cropping: true
     }).then(img => {
       setSource({
